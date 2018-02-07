88bf637f3094b31645d05d805a32f6bee6a4e862
Paul Cozma <contact@sm-design.ro>
2018-02-07 23:21:11 +0200

    thank you

---
+++ pages/thank-you.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <div class="grid">
+      <h1 class="display-4">Thank you!</h1>
+        <div class="display-2">We recieved your message and we are making sure you will get the response you need as soon as posible!</div>
+  </div>
+  </div>
+</template>
+<style scoped>
+.grid{
+    display: flex;
+    flex-flow: wrap row;
+    justify-content: center;
+    align-items: center;
+    color:#fff;
+}
+</style>
