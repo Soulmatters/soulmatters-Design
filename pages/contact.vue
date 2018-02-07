422b1a74dd94b53440fc915357e5df515caf8f07
Paul Cozma <contact@sm-design.ro>
2018-02-07 22:42:31 +0200

    lets see

--- pages/contact.vue
+++ pages/contact.vue
@@ -19,20 +19,7 @@
   </gmap-map>
       </div> -->
       <div class="contact-data">
-        <form name="test" netlify>
-  <p>
-    <label>Your Name: <input type="text" name="name"></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email"></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+       
       </div>
       <div class="contact-form">
         <template>
