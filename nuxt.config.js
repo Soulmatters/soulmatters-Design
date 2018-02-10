ea6b995d3c0371d65035ba328187a03065d756d3
Paul Cozma <contact@sm-design.ro>
2018-02-10 16:53:35 +0200

    manifest

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   loading: { color: '#2e7d32', height:'10px' },
-  modules: [ '@nuxtjs/offline', "nuxtent", "nuxt-netlify-cms"  ],
+  modules: [ '@nuxtjs/offline','@nuxtjs/manifest', "nuxtent", "nuxt-netlify-cms"  ],
   manifest: {
     name: 'SM-Design.ro',
     lang: 'ro',   
