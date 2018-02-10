80d6402f9cd60e30382a95ece75660a92f11ac09
Paul Cozma <contact@sm-design.ro>
2018-02-10 17:48:40 +0200

    terms

---
+++ pages/terms.vue
@@ -0,0 +1,29 @@
+
+<template>
+
+  <div>
+   
+<h1>{{page.title}}</h1>
+      
+  </div>
+</template>
+<script>
+export default {
+  async asyncData ({ app, route }) {
+    return {
+      page: await app.$content('/pages').get(route.path),
+
+    }
+  },
+   
+
+  
+}
+</script>
+<style scoped>
+h2{
+  color:#fff;
+  text-align: center;
+  padding-top: 50px;
+}
+</style>
