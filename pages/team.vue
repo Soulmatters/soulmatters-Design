4b023b7ee00fb9fecd0b9f0c369c1771358fc335
Paul Cozma <contact@sm-design.ro>
2018-02-11 13:12:51 +0200

    changes

--- pages/team.vue
+++ pages/team.vue
@@ -2,25 +2,14 @@
   <div>
       <div class="grid">
         <div class="about-us primary-font">
-                <h1 class="secondary-font">About us</h1>
-               <p>SoulMatters Design it's a company that offers web design services. 
-                   We use the latest technologies in web development and we successfully meet the most demanding requirements of our customers.</p>
-
-<p>In our opinion, the first impression matters and the soul as well - fact which it can be observed in our work. It's Soulmatters Design and we create web pages with a unique, functional and user-friendly design.
-
-We take care of creating the right image and functionality for every project and guiding our clients with valuable information.</p>
-
-<p>In creating a website, our team use the latest web development technologies and this will drive our projects to new perspectives.</p>
-<p> Soulmaters Design is a company with a young team, which constantly want to bring good changes for world wide web. Our team is ready to courageously accept the new challenges in web development.</p>
-
-<p>Our team is extremely happy to "encode" a dream and then to reveal it as a successful website. Soulmatters Design is working with pleasure for it's customers and it's obtaining results through perseverence. </p>
-<p>Now, among us, you can have a Web Progressive Application. Give to yourself a chance to have a performant website and the results won't be late.</p>
+                <h1 class="secondary-font">{{team.title}}</h1>
+              <div>{{team.description}}</div>
         </div>
-        <v-card class="items" v-for="item in team" :key="item.name">
-          <v-card-media  height="500px" :src="item.image"></v-card-media>
+        <v-card class="items" v-for="item in team.team" :key="item.firstname">
+          <v-card-media  height="500px" :src="item.image.replace(/static\//gi, '')"></v-card-media>
           <v-card-title primary-title>
             <div>
-          <div class="primary-font headline ">{{item.name}}</div>
+          <div class="primary-font headline ">{{item.firstname  + ' ' +item.lastname}}</div>
           <span class="gre--text">{{item.position}}</span>
           </div>
           <div class="description primary-font">{{item.description}}</div>
@@ -92,35 +81,10 @@ h3{
 
 <script>
 export default {
-  data(){
+async asyncData ({ app, route }) {
     return {
-      team:[
-        {
-          name:"Andra Niță",
-          description: `My detail oriented skills have always been appreciated and were a great help to our team in every step of a project. I like to "code" web pages. It is a creative work that always come with new challenges. Also, I like to create written content for websites - adapting it to our client's requirements and optimizing search engine sites. I'll never stop earning because I'll never stop learning!`,
-          image:'/uploads/andra.jpg',
-          position: 'Fullstack developer, CEO',
-          social: [
-            {name:"facebook" , link:"https://www.facebook.com/soulmatters.ro", color:"#4267b2"},
-            {name:"google-plus" , link:"https://plus.google.com/+AndraNitasoulmatters", color:"#db4437"},
-            {name:"linkedin" , link:"https://www.linkedin.com/in/nita-andra/", color:"#0077B5"},
-          ]
-        },
-        {
-          name:"Paul Cozma",
-          description: `Web design is one of the most important areas I have chosen to pursue. It is an exciting domain, in a continuous evolution, which offers new perspectives for those who want to represent themselves in a professional manner.
-I can fulfill all my client's requirements in difficult projects. I help them in web design matters and putting all my soul for great results.`,
-          image:'/uploads/paul.jpg',
-          position: 'Fullstack developer, co-founder',          
-          social: [
-            {name:"twitter" , link:"https://twitter.com/PaulSoulMatters", color:"#1da1f2"},
-            {name:"facebook" , link:"https://www.facebook.com/CozmaPaulSM?", color:"#4267b2"},
-            {name:"google-plus" , link:"https://plus.google.com/+PaulCozma" ,color:"#db4437"},
-            {name:"linkedin" , link:"https://www.linkedin.com/in/paul-cozma-68420b89/", color:"#0077B5"},
-            
-          ]
-        }
-      ]
+      team: await app.$content('/pages').get(route.path),
+
     }
   },
    head () {
